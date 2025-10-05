9d60aa2d366e020269908c9b97f55467f17e61b8
Noibi <noibi@Noibis-MacBook-Air.local>
2025-10-05 14:49:40 +0300

    Cleanup: Poistettu kaikki turhat tiedostot - vain ultra_simple_bot ja työkalut jäljellä

--- update_env_api_key.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Update API key index in .env
-sed -i '' 's/LIGHTER_API_KEY_INDEX=2/LIGHTER_API_KEY_INDEX=0/g' .env
-echo "Updated LIGHTER_API_KEY_INDEX to 0"
--- update_env_api_key_index.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Update API key index to 10 in .env
-sed -i '' 's/LIGHTER_API_KEY_INDEX=0/LIGHTER_API_KEY_INDEX=10/g' .env
-echo "Updated LIGHTER_API_KEY_INDEX to 10"
